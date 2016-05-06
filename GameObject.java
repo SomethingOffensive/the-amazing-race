3c6d87622b65bf95a9a1d7ad9bb6310222030b57
Patrick Higgins <higgp153@hsd.k12.or.us>
2016-05-06 13:14:50 -0700

    Added the REAL GameObject class
    much better

--- GameObject.java
+++ GameObject.java
@@ -5,6 +5,9 @@
  * @author Patrick Higgins 
  * @version 1.0
  */
+
+import java.awt.Graphics;
+import java.awt.Image;
 public class GameObject
 {
     // xy position of object
@@ -12,17 +15,22 @@ public class GameObject
     protected float y;
     
     //xy velocity of object
-    
     protected float velx;
     protected float vely;
+    
+    //image to represent object
+    private Image image;
 
     /**
      * Constructor for objects of class GameObject
      */
-    public GameObject(String img,float x, float y)
+    public GameObject(Image image, float x, float y)
     {
         // initialise instance variables
-        x = 0;
+        this.x = x;
+        this.y = y;
+        
+        this.image = image;
     }
 
 }
