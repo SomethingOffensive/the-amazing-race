b6b20e4ec5076ae7688096c6af9869611931a068
Patrick Higgins <higgp153@hsd.k12.or.us>
2016-05-06 13:13:23 -0700

    Added GameObject class
    Root class for all objects - only has a constructor so far

---
+++ GameObject.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class GameObject here.
+ * 
+ * @author Patrick Higgins 
+ * @version 1.0
+ */
+public class GameObject
+{
+    // xy position of object
+    protected float x;
+    protected float y;
+    
+    //xy velocity of object
+    
+    protected float velx;
+    protected float vely;
+
+    /**
+     * Constructor for objects of class GameObject
+     */
+    public GameObject(String img,float x, float y)
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+}
