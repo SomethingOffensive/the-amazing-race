1497936a7e53a917fefa022c3ee350986c051e3a
Patrick Higgins <higgp153@hsd.k12.or.us>
2016-05-16 10:27:32 -0700

    Added Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.JPanel;
+
+public class Enemy extends GameObject
+{
+    public Enemy(Image image, float x, float y, int w, int l) {
+    super(image, x, y, w, l);
+    }
+}
+
+  
