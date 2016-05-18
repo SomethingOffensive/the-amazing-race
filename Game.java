f0dca285788625d8efcd2d20441c7c605604f5ac
Nate Colbert <hero.hoolaf@gmail.com>
2016-05-18 11:55:25 -0700

    Added some functionality to Level 1 and Game.java

--- Game.java
+++ Game.java
@@ -1,31 +1,16 @@
-
-/**
- * Write a description of class Game here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Game
 {
     public static int score;
     public static Board[] levels;
-    /**
-     * Constructor for objects of class Game
-     */
     public Game()
     {
 
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
     public static void main()
     {
-        
+        Level_1 level1 = new Level_1();
+        level1.drawBoard();
     }
     
     public static void doLevel(Board level){
--- Level_1.java
+++ Level_1.java
@@ -4,19 +4,27 @@ import java.io.*;
 import java.net.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
- 
+import java.io.File; 
 
 public class Level_1 extends Board 
 { 
-    public String boardFileName;
-    ImageLoader loader = new ImageLoader();    
+    ImageLoader loader = new ImageLoader();
+   
     public Level_1()
     {
         initBoard();
     }
     
-    private void drawBoard() {
-        BufferedImage background = loader.loadImage(boardFileName);
+    public void drawBoard()
+    {
+        String boardFileName = DirFinder.findDir("placeholder.jpg");	
         
+        BufferedImage background = loader.loadImage(boardFileName); 
+       
+        System.out.println(boardFileName); // TEST - to be removed
+        
+        if (background != null) {
+            System.out.println("Image Loaded."); // Also test
+        }
     }
 }
