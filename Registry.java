ca43417f98d4fb6e1443b36c279653f9f0b3234b
Patrick Higgins <quickpanic@me.com>
2016-05-19 08:57:07 -0700

    Update Registry.java

--- Registry.java
+++ Registry.java
@@ -14,10 +14,15 @@ public class Registry
     // instance variables - replace the example below with your own
     private static ArrayList<GameObject> allObjects = new ArrayList<GameObject>();
     
+    public static ArrayList<GameObject> allGameObjects() {
+        return allObjects;
+    }
     public static void register(GameObject obj) {
         allObjects.add(obj);
     }
-    
+    public static void remove(GameObject obj) {
+        allObjects.remove(obj);
+    }
     public static void tickAll(float deltaTime) {
         for (GameObject obj : allObjects) {
             obj.tick(deltaTime);
