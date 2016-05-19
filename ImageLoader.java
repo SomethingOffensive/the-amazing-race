06a8413041fda7518868262590033fdd2e5f0dd2
Nate Colbert <hero.hoolaf@gmail.com>
2016-05-19 09:07:00 -0700

    'Fixed' ImageLoader

--- ImageLoader.java
+++ ImageLoader.java
@@ -10,27 +10,6 @@ public class ImageLoader
     public BufferedImage loadImage(String fileName)
     {
         BufferedImage image = null;
-        try
-        {
-            URL url = getClass().getResource(fileName);
-            image = ImageIO.read(url);
-        }
-        catch(MalformedURLException mue)
-        {
-            System.err.println("url: " + mue.getMessage());
-        }
-        catch(IllegalArgumentException iae)
-        {
-            System.err.println("arg: " + iae.getMessage());
-        }
-        catch(IOException ioe)
-        {
-            System.err.println("read: " + ioe.getMessage());
-        }
-        if(image == null)
-        {
-            System.out.println("unable to load image");
-        }
-        return image;
+
     }    
 }
