de62bf42cea5e4998100bb1d292757903a76235e
Nate Colbert <hero.hoolaf@gmail.com>
2016-05-19 12:34:34 -0700

    Added drawing functionality

--- Level_1.java
+++ Level_1.java
@@ -7,19 +7,20 @@ import javax.swing.*;
 import java.io.File; 
 
 public class Level_1 extends Board 
-{ 
-    ImageLoader loader = new ImageLoader();
-   
+{
     public Level_1()
     {
         initBoard();
     }
     
-    public void drawBoard()
+    public void doDrawing(Graphics g)
     {
         String boardFileName = DirFinder.findDir("placeholder.jpg");	
         
-        BufferedImage background = loader.loadImage(boardFileName); 
+        BufferedImage background = loadImage(boardFileName); 
+        setBackground(background);
+        super.doDrawing(g);
+        g.drawImage(0,0,100,100,this);
        
         System.out.println(boardFileName); // TEST - to be removed
         
