b58b077316289f791183ca8a64799c9ea41cffea
Patrick Higgins <higgp153@hsd.k12.or.us>
2016-06-02 10:51:34 -0700

    added liberals

---
+++ Liberal.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.JPanel;
+
+public class Liberal extends Enemy
+{
+
+    /**
+     * Constructor for objects of class Liberal
+     */
+    public Liberal(Image image, float x, float y, int w, int l) {
+        super(image, x, y, w, l);    
+    }
+    
+    protected void tick(float deltaTime) {
+        x += velx;
+        y += vely;
+    }
+    
+    public void moveToTarget(GameObject target) {
+       
+        float dx = x - target.x;
+        float dy = y - target.y;
+        
+        double angle = Math.atan(dy/dx);
+        
+        velx = (float)Math.cos(angle) * velocity;
+        vely = (float)Math.sin(angle) * velocity;
+    }
+}
